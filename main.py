ab361a33294c2935d26a89fba60d246420774810
James Rogers <102842567+DuckyDuckerson@users.noreply.github.com>
2024-07-08 10:08:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from rps import RPS #type: ignore
-from hangman import Hangman #type: ignore
-from tictactoe import TicTacToe #type: ignore
+from rps import RPS  # type: ignore
+from hangman import Hangman  # type: ignore
+from tictactoe import TicTacToe  # type: ignore
 from support import print_message as pm
+
 
 rps = RPS()
 hangman = Hangman()
@@ -11,19 +12,21 @@ games = [rps, hangman, tictactoe]
 
 def main():
     pm("Welcome to the Games console! What game would you like to play?", 2, 1)
+
     i = 1
+
     for game in games:
         print(f"{i}. {game.name}")
         i += 1
     game_choice = input("Input the game number for your chosen game. ")
-    
+
     chosen_game = games[int(game_choice) - 1]
     chosen_game.playing = True
 
-    while chosen_game.playing:      
+    while chosen_game.playing:
         chosen_game.game_loop()
-        
     main()
+
 
 if __name__ == "__main__":
     main()
