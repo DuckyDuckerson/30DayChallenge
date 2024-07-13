93e1393f6d389125dd35f6015624c4ae848eb1bc
James Rogers <102842567+DuckyDuckerson@users.noreply.github.com>
2024-07-12 23:31:05 -0700

    Update ducky.py

--- playground/ducky.py
+++ playground/ducky.py
@@ -1,5 +1,5 @@
-from support.tools import print_message as pm
-
+thirdy = __import__('30DayChallenge.support.tools')
+from thirdy import pm
 
 a = 2
 b = 4
@@ -14,3 +14,4 @@ list1 = [d, e, f]
 list2 = list0 + list1
 
 pm(f'{list2}', 2, 1)
+
